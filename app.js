36fc84959719c80b14b213df0f14391b246871d0
lais-mello <laisflavio.silva@gmail.com>
2025-08-07 15:08:09 -0300

    Criar funções adicionarAmigo, atualizarListaDeAmigos, sortearAmigo e limparCampo

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
+let listaDeAmigos = [];
+function adicionarAmigo() {
+  let capturaNomes = document.querySelector("input").value;
+
+  if ((capturaNomes != null) && (capturaNomes != "")) {
+    listaDeAmigos.push(capturaNomes);
+    console.log(listaDeAmigos);
+    atualizarListaDeAmigos();
+  } else {
+    alert("Por favor, insira um nome.");
+  } limparCampo();
+}
+
+function atualizarListaDeAmigos() {
+  let lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+
+  for (let i = 0; i < listaDeAmigos.length; i++) {
+    novoItem = document.createElement("li");
+    novoItem.innerHTML = listaDeAmigos[i];
+    lista.appendChild(novoItem);
+  }
+}
+
+function sortearAmigo() {
+  let amigoEscolhido = Math.floor(Math.random() * listaDeAmigos.length);
+  let amigoSorteado = listaDeAmigos[amigoEscolhido];
+
+  listaSorteado = document.getElementById("resultado");
+  listaSorteado.innerHTML = "";
+
+  if ((amigoSorteado != "") && (amigoSorteado != null)) {
+    novoItemSorteado = document.createElement("li");
+    novoItemSorteado.innerHTML = amigoSorteado;
+    listaSorteado.appendChild(novoItemSorteado)
+  } else {
+    listaSorteado.innerHTML = "";
+  }
+}
+sortearAmigo();
+function limparCampo() {
+  capturaNomes = document.querySelector("input");
+  capturaNomes.value = "";
+}
 
